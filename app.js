79158df165db54b2c0469829586cf4e037deefa3
Hafiz sharim sohail <sharimsohail17@gmail.com>
2024-09-29 12:24:13 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,57 @@
+//addition
+var a = 5;
+var b = 5;
+var c = a + b;
+console.log(c);
+
+//subtraction
+var a = 10;
+var b = 5;
+var c = a - b;
+console.log(c);
+
+//multiplication
+var a = 5;
+var b = 5;
+var c = a * b;
+console.log(c);
+
+//division
+var a = 5;
+var b = 5;
+var c = a / b;
+console.log(c);
+
+//modulus
+var a = 5;
+var b = 10;
+var c = a % b;
+console.log(c);
+
+//Exponentiation
+var a = 5;
+var b = 4;
+var c = a ** b;
+console.log(c);
+
+//Prefix Increment
+var a = 5;
+var b = ++a;
+console.log(b);
+
+//Postfix Increment
+var a = 5;
+var b = a++;
+console.log(b);
+
+//Prefix decrement
+var a = 5;
+var b = --a;
+console.log(b);
+
+//Postfix decrement
+var a = 5;
+var b = a--;
+console.log(b);
+
+
